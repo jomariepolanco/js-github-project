6036fdc4b9d93e9557d24feb58598b565263cb00
jomariepolanco <jomariepolanco@gmail.com>
2020-11-21 10:29:22 -0500

    third done

--- js/index.js
+++ js/index.js
@@ -8,10 +8,24 @@ form.addEventListener("submit", event => {
     searchFetchRequest(input)
 })
 
+userList.addEventListener("click", event => {
+    console.log(event)
+    if (event.target.matches("h3")) {
+        const user = event.target.closest("h3")
+        repoFetchRequest(user)
+    }
+})
+
 const searchFetchRequest = searchValue => {
     return fetch(`https://api.github.com/search/users?q=${searchValue}`)
         .then(response => response.json())
         .then(gitHubMatches => renderSearchResults(gitHubMatches.items))
+}
+
+const repoFetchRequest = (user) => {
+    return fetch(`https://api.github.com/users/${user.textContent}/repos`)
+        .then(response => response.json())
+        .then(repos => console.log(repos))
 }
 
 const renderSearchResults = (data) => {
@@ -19,12 +33,13 @@ const renderSearchResults = (data) => {
         const userH3 = document.createElement("h3")
         userH3.textContent = user.login
         userH3.dataset.id = user.id 
+        const userP = document.createElement("p")
         const userImg = document.createElement("img")
         userImg.src = user.avatar_url 
         const userLink = document.createElement("a")
         userLink.href = user.html_url 
         userLink.textContent = user.html_url 
-        userH3.append(userLink, userImg)
-        userList.append(userH3)
+        userP.append(userLink, userImg)
+        userList.append(userH3, userP)
     })
 }
