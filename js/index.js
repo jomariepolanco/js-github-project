50be77b77947de5de9a6cd7a1a156ebdbcb38ced
jomariepolanco <jomariepolanco@gmail.com>
2020-11-21 10:19:20 -0500

    2nd deliverable done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const form = document.querySelector("#github-form")
 const userList = document.querySelector("#user-list")
-console.log(userList)
+// console.log(userList)
 
 form.addEventListener("submit", event => {
     event.preventDefault()
@@ -11,5 +11,20 @@ form.addEventListener("submit", event => {
 const searchFetchRequest = searchValue => {
     return fetch(`https://api.github.com/search/users?q=${searchValue}`)
         .then(response => response.json())
-        .then(gitHubMatches => console.log(gitHubMatches))
+        .then(gitHubMatches => renderSearchResults(gitHubMatches.items))
+}
+
+const renderSearchResults = (data) => {
+    data.forEach(user => {
+        const userH3 = document.createElement("h3")
+        userH3.textContent = user.login
+        userH3.dataset.id = user.id 
+        const userImg = document.createElement("img")
+        userImg.src = user.avatar_url 
+        const userLink = document.createElement("a")
+        userLink.href = user.html_url 
+        userLink.textContent = user.html_url 
+        userH3.append(userLink, userImg)
+        userList.append(userH3)
+    })
 }
